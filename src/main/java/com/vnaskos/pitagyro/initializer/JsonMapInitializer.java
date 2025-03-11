5b1103a366ed4c0cd242a1394f4040d18e5a42db
Vasilis Naskos <vnaskos@gmail.com>
2025-03-11 22:29:46 +0100

    Update json-io to 4.30.0

--- src/main/java/com/vnaskos/pitagyro/initializer/JsonMapInitializer.java
+++ src/main/java/com/vnaskos/pitagyro/initializer/JsonMapInitializer.java
@@ -1,11 +1,12 @@
 package com.vnaskos.pitagyro.initializer;
 
-import com.cedarsoftware.util.io.JsonReader;
+import com.cedarsoftware.io.JsonReader;
 import com.vnaskos.pitagyro.location.Location;
-import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,13 +17,13 @@ import java.util.logging.Logger;
 public class JsonMapInitializer extends MapInitializer {
 
     protected Location initialLocation;
-    
+
     @Override
     public void initialize() {
         try {
-            InputStream is = new FileInputStream("test_map.json");
-            HashMap<String, Object> jsonArgs = new HashMap<>();
-            initialLocation = (Location) JsonReader.jsonToJava(is, jsonArgs);
+            InputStream is = Files.newInputStream(Paths.get("test_map.json"));
+            JsonReader jsonReader = new JsonReader(is, null);
+            initialLocation = jsonReader.readObject(Location.class);
         } catch (IOException ex) {
             Logger.getLogger(JsonMapInitializer.class.getName()).log(Level.SEVERE, null, ex);
         }
--- src/main/java/com/vnaskos/pitagyro/initializer/TestMapInitializer.java
+++ src/main/java/com/vnaskos/pitagyro/initializer/TestMapInitializer.java
@@ -1,6 +1,8 @@
 package com.vnaskos.pitagyro.initializer;
 
-import com.cedarsoftware.util.io.JsonWriter;
+import com.cedarsoftware.io.JsonWriter;
+import com.cedarsoftware.io.WriteOptions;
+import com.cedarsoftware.io.WriteOptionsBuilder;
 import com.vnaskos.pitagyro.PitaGyro;
 import com.vnaskos.pitagyro.gate.GateBuilder;
 import com.vnaskos.pitagyro.item.ContainerItem;
@@ -13,9 +15,10 @@ import com.vnaskos.pitagyro.location.Location;
 import com.vnaskos.pitagyro.location.Room;
 import com.vnaskos.pitagyro.player.Enemy;
 import com.vnaskos.pitagyro.util.Pair;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.HashMap;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,7 +27,7 @@ import java.util.logging.Logger;
  * @author Vasilis Naskos
  */
 public class TestMapInitializer extends MapInitializer {
-    
+
     @Override
     public void initialize() {
         //Init rooms
@@ -32,9 +35,9 @@ public class TestMapInitializer extends MapInitializer {
         Room r2 = new Room("stin toualeta", "Vlepo mia merida gyro");
         Room r3 = new Room("sta nisia Cayman (Opa opa! ela piso pou pas)", "Op sa na vlepo mia pita");
         Room r4 = new Room("sto ouzeri tou mpelami", "Hmmmm");
-        
+
         startLocation = r1;
-        
+
         //Init items
         Item key = new PassiveItem("pirouni", "Key to unlock door 1");
         Item pita = new PassiveItem("pita", "PITAAAA");
@@ -43,50 +46,46 @@ public class TestMapInitializer extends MapInitializer {
         ContainerItem psigeio = new ContainerItem("psigeio", "ena aplo psigeio", apola);
         UsableItem<Integer> maxairi = new UsableItem<>("maxairi", Usable.UseCase.ATTACK, 10);
         UsableItem<Integer> patates = new UsableItem<>("patates", Usable.UseCase.HEALTH, 5);
-        
+
         r1.addItem(key.getName(), key);
         r1.addItem(maxairi.getName(), maxairi);
         r3.addItem(pita.getName(), pita);
         r2.addItem(gyros.getName(), gyros);
         r4.addItem(psigeio.getName(), psigeio);
         r4.addItem(patates.getName(), patates);
-        
+
         Enemy e3 = new Enemy("o kardiologos", 30, 5, 0);
         r3.setRoomCharacater(e3);
-        
-        //Locked Door r1 - r2 
+
+        //Locked Door r1 - r2
         new GateBuilder(
                 new Pair(Direction.KATO, r1),
                 new Pair(Direction.PANO, r2))
                 .key(key).buildAndAttach();
-        
+
         //Door r1 r3
         new GateBuilder(
                 new Pair(Direction.PANO, r1),
                 new Pair(Direction.KATO, r3))
                 .buildAndAttach();
-        
+
         // Door r1 r3
         new GateBuilder(
                 new Pair(Direction.DEXIA, r1),
                 new Pair(Direction.ARISTERA, r4))
                 .buildAndAttach();
-        
-        //exportMapToJSON(startLocation);
-    }
-    
-    private static void exportMapToJSON(Location location) {
-        HashMap<String, Object> jsonArgs = new HashMap<>();
-        jsonArgs.put("PRETTY_PRINT", true);
-        String json = JsonWriter.objectToJson(location, jsonArgs);
 
-        try (PrintWriter out = new PrintWriter("test_map.json")) {
-            out.print(json);
-        } catch (FileNotFoundException ex) {
+        exportMapToJSON(startLocation);
+    }
+
+    private static void exportMapToJSON(Location location) {
+        WriteOptions writeOptions = new WriteOptionsBuilder().prettyPrint(true).build();
+
+        try (JsonWriter jsonWriter = new JsonWriter(Files.newOutputStream(Paths.get("test_map.json")), writeOptions)) {
+            jsonWriter.write(location);
+        } catch (IOException ex) {
             Logger.getLogger(PitaGyro.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        System.out.println(json);
     }
-    
+
 }
